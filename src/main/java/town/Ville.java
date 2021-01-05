adf53ff47814a677d964ac4d839cba07ceb55012
Nathanael-Lefevre <nathanael.lefevre@alumni.univ-avignon.fr>
2021-01-05 10:53:01 +0100

    push intermédiaire

--- src/main/java/town/Ville.java
+++ src/main/java/town/Ville.java
@@ -1,14 +1,20 @@
 package town;
 
+import java.lang.reflect.Type;
 import java.util.HashMap;
 
 public class Ville {
     static HashMap<Class, Boolean> batiments = new HashMap<Class, Boolean>();
-    private Case[][] test;
+    private Case[][] listeCase;
 
     public Ville()
     {
-        test = new Case[10][10];
+        listeCase = new Case[10][10];
+    }
+
+
+    public void addCase(int ligne, int colonne, String classType) throws Exception {
+        this.listeCase[ligne][colonne] = (Case) Class.forName(classType).newInstance();
     }
 
     public static void setBatiments(Class c, Boolean bool) {
