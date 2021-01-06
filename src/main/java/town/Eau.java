d4360d172289f100c737cb6b8795bbc515575313
Nathanael-Lefevre <nathanael.lefevre@alumni.univ-avignon.fr>
2021-01-06 16:16:33 +0100

    Equilibrage des pertes pour ameliorer la jouabilité

--- src/main/java/town/Eau.java
+++ src/main/java/town/Eau.java
@@ -9,8 +9,8 @@ public class Eau extends CaseDeDeplacement{
 
     public void suru(Personnage pers){
         System.out.println("Tous en maillot de bain !");
-        pers.updateValue("hydratation", (int) (-10*pers.getFacteurDePerte()));
-        pers.updateValue("satiete", (int) (-10*pers.getFacteurDePerte()));
+        pers.updateValue("hydratation", (int) (-3*pers.getFacteurDePerte()));  // 3 au lieu de 10
+        pers.updateValue("satiete", (int) (-3*pers.getFacteurDePerte()));  // 3 au lieu de 10
     }
 
     @Override
