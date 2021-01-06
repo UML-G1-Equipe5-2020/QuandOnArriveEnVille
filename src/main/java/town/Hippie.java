75a6b1fb5e425cc1ec3dfd3f8c4cc0c59aa80b0b
Sébastien ROCCA <sebastien.rocca@alumni.univ-avignon.fr>
2021-01-06 22:18:36 +0100

    Ajout de commentaires dans les Classes Standard, Hippie, HommePresse et Personnage.

--- src/main/java/town/Hippie.java
+++ src/main/java/town/Hippie.java
@@ -1,7 +1,7 @@
 package town;
 
 public class Hippie extends Personnage {
-    public Hippie(Case house, boolean permis, boolean velo, boolean maillot) {
+    public Hippie(Case house, boolean permis, boolean velo, boolean maillot) { //définie les attributs d'une instance du personnage HommePresse
         super(house, permis, velo, maillot);
         this.maxVie = 75;
         this.maxHydratation = 50;
@@ -16,16 +16,16 @@ public class Hippie extends Personnage {
 
     }
 
-    public void updateValue(String barre, int valeur ) {
-        if (!barre.equals("moral")) {
-            super.updateValue(barre, valeur);
+    public void updateValue(String barre, int valeur ) { //on ajoute une spécifité pour ce personnage pour la mise a jour de ses attributs vitaux
+        if (!barre.equals("moral")) { //il ne peut pas perdre du moral peu importe ce qu'il se passe, donc la barre de moral ne peut pas être impactée
+            super.updateValue(barre, valeur); //on utilise la methode "updateValue()" de la classe Personne.
         }
     }
-
+    //définie une spécificité du personnage lorsqu'il se déplace en plus de la methode "seDeplacer()" dans la classe Personnage
     public boolean seDeplacer(String direction, Ville ville){
-        boolean deplace = super.seDeplacer(direction, ville);
+        boolean deplace = super.seDeplacer(direction, ville); //on utilise la méthode "seDeplacer()" de la classe Personne
         if(deplace){
-            this.updateValue("vie", -2);
+            this.updateValue("vie", -2); //On ajoute la spécificité: Le personnage type hippie perd 2 points de vie lorsqu'il se déplace d'une case
         }
         return deplace;
     }
--- src/main/java/town/Standard.java
+++ src/main/java/town/Standard.java
@@ -1,7 +1,7 @@
 package town;
 
 public class Standard extends Personnage {
-    public Standard(Case house, boolean permis, boolean velo, boolean maillot) {
+    public Standard(Case house, boolean permis, boolean velo, boolean maillot) { //définie les attributs d'une instance du personnage HommePresse
         super(house, permis, velo, maillot);
         this.maxVie = 75;
         this.maxHydratation = 75;
@@ -15,11 +15,11 @@ public class Standard extends Personnage {
         this.moral = this.maxMoral;
     }
 
-
+    //définie une spécificité du personnage lorsqu'il se déplace en plus de la methode "seDeplacer()" dans la classe Personnage
     public boolean seDeplacer(String direction, Ville ville){
-        boolean deplace = super.seDeplacer(direction, ville);
+        boolean deplace = super.seDeplacer(direction, ville); //on utilise la méthode "seDeplacer()" de la classe Personne
         if(deplace){
-            this.updateValue("vie", -1);
+            this.updateValue("vie", -1); //pour chaque déplacement, le personnage Standard
         }
         return deplace;
     }
